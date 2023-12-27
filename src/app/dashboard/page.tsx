22b947f956b0abc6d6d713e8909b630ccfa51b35
winnyDiese <winnydiese@gmail.com>
2023-12-27 16:12:16 +0100

    Creating nav-bar

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function DashboardPage() {
+    return <div>Dashbord page</div>
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
+import Nav from './components/Nav'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +15,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="min-h-screen flex justify-center items-center p-24">{children}</body>
+      <body className="min-h-screen ">
+        <Nav />
+        <div className='flex justify-center items-center p-24'>
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
