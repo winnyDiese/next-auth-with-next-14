c9eb2a2497229936aa1df7d846a02760e37e6554
winnyDiese <winnydiese@gmail.com>
2023-12-28 07:58:36 +0100

    Finish to configurate xata db with this project

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,13 @@
+import { getXataClient } from '@/xata';
 import React from 'react';
 
-export default function DashboardPage() {
-    return <div>Dashbord page</div>
+export default async function DashboardPage() {
+    const xataClient = getXataClient
+    const folders = await xataClient.db.folders.getMany()
+    return <div>
+        <h1>Dashboard page</h1>
+        {folders.map(folder=> (
+            <p key={folder.id}>{folder.name}</p>
+        ))}
+    </div>
 }
